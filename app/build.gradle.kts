2f37d6d6c7f92dad1196ef728dd8d1aee2c8de96
ahanafrifat <ahanafrifat@gmail.com>
2025-08-18 00:59:32 +0200

    Updated all the library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.ahanafrifat.yourplants"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.ahanafrifat.yourplants"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
