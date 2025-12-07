3d239d452200d4b4522bafe3e8c4fcbf06d23b09
ahanafrifat <ahanafrifat@gmail.com>
2025-12-07 02:35:21 +0100

    EchoPlaybackButton done

--- app/src/main/java/com/ahanafrifat/yourplants/enhos/presentation/components/EchoPlaybackButton.kt
+++ app/src/main/java/com/ahanafrifat/yourplants/enhos/presentation/components/EchoPlaybackButton.kt
@@ -1,42 +1,65 @@
 package com.ahanafrifat.yourplants.enhos.presentation.components
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.ahanafrifat.yourplants.R
+import com.ahanafrifat.yourplants.core.presentation.designsystem.theme.Pause
 import com.ahanafrifat.yourplants.core.presentation.designsystem.theme.YourPlantsTheme
+import com.ahanafrifat.yourplants.core.presentation.util.defaultShadow
 import com.ahanafrifat.yourplants.enhos.presentation.models.MoodUi
 import com.ahanafrifat.yourplants.enhos.presentation.models.PlaybackState
 
 @Composable
 fun EchoPlaybackButton(
     playbackState: PlaybackState,
-    onPlayClick: ()-> Unit,
-    onPauseClick: ()-> Unit,
+    onPlayClick: () -> Unit,
+    onPauseClick: () -> Unit,
     colors: IconButtonColors,
     modifier: Modifier = Modifier
-){
+) {
     FilledIconButton(
-        onClick = when (playbackState){
+        onClick = when (playbackState) {
             PlaybackState.PLAYING -> onPauseClick
             PlaybackState.PAUSED,
             PlaybackState.STOPPED -> onPlayClick
-        }
-    ) { }
+        },
+        colors = colors,
+        modifier = modifier.defaultShadow()
+    ) {
+        Icon(
+            imageVector = when(playbackState){
+                PlaybackState.PLAYING -> Icons.Filled.Pause
+                PlaybackState.PAUSED,
+                PlaybackState.STOPPED -> Icons.Filled.PlayArrow
+            },
+            contentDescription = when(playbackState){
+                PlaybackState.PLAYING -> stringResource(R.string.playing)
+                PlaybackState.PAUSED -> stringResource(R.string.paused)
+                PlaybackState.STOPPED -> stringResource(R.string.stopped)
+            }
+        )
+    }
 }
 
 @Preview
 @Composable
-private fun EchoPlaybackButtonPreview(){
+private fun EchoPlaybackButtonPreview() {
     YourPlantsTheme {
         EchoPlaybackButton(
             playbackState = PlaybackState.PLAYING,
             onPlayClick = {},
             onPauseClick = {},
             colors = IconButtonDefaults.filledIconButtonColors(
-                containerColor = MoodUi.SAD.colorSet.desaturated,
+                containerColor = MaterialTheme.colorScheme.surface,
                 contentColor = MoodUi.SAD.colorSet.vivid
             )
         )
