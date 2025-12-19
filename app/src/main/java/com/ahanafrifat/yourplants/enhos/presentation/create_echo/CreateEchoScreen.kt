9eaade1ad9a225a179337aa41b321f1eeccaab47
ahanafrifat <ahanafrifat@gmail.com>
2025-12-19 23:45:05 +0100

    working on CreateEcho

--- app/src/main/java/com/ahanafrifat/yourplants/enhos/presentation/create_echo/CreateEchoScreen.kt
+++ app/src/main/java/com/ahanafrifat/yourplants/enhos/presentation/create_echo/CreateEchoScreen.kt
@@ -3,7 +3,16 @@ package com.ahanafrifat.yourplants.enhos.presentation.create_echo
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.ahanafrifat.yourplants.core.presentation.designsystem.theme.YourPlantsTheme
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun CreateEchoRoot(
+    viewModel: CreateEchoViewModel = koinViewModel()
+){
+    val state =viewModel.state.collectAsStateWithLifecycle()
+}
 
 @Composable
 fun CreateEchoScreen(
